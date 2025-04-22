f93a77563ecf08f5687efde816cb09bac42abff7
arifnoviarrizal <78676204+noviarrizal@users.noreply.github.com>
2025-04-22 19:45:12 +0700

    feat(agent component): adding mock data for transcript

---
+++ components/Agent/Agent.tsx
@@ -0,0 +1,98 @@
+"use client";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+enum CallStatus {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
+}
+
+const Agent = ({ userName }: AgentProps) => {
+  const callStatus = CallStatus.ACTIVE;
+  const isSpeaking = true;
+  const messages = [
+    "Whats your name?",
+    "My name is jane doe, nice to meet you!",
+  ];
+
+  const lastMessage = messages[messages.length - 1];
+
+  return (
+    <>
+      <div className="call-view">
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="profile-image"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak" />}
+          </div>
+          <h3>AI Interview</h3>
+        </div>
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/user-avatar.png"
+              alt="profile-image"
+              width={539}
+              height={539}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+      <div className="transcript-border">
+        <div className="transcript">
+          <p
+            key={""}
+            className={cn(
+              "transition-opacity duration-500 opacity-0",
+              "animate-fadeIn opacity-100"
+            )}
+          >
+            last Massage
+          </p>
+        </div>
+      </div>
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p key={lastMessage}>{lastMessage}</p>
+          </div>
+        </div>
+      )}
+      <div className="w-full flex justify-center">
+        {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call" onClick={() => {}}>
+            <span
+              className={cn(
+                "absolute animate-ping rounded-full opacity-75",
+                callStatus !== "CONNECTING" && "hidden"
+              )}
+            />
+
+            <span className="relative">
+              {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                ? "Call"
+                : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect" onClick={() => {}}>
+            End
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Agent;
